7d59e1f63472312313fb117b43b9010c84e09d33
Hassan Abdinur <hassan.abdinur123@gmail.com>
2022-01-13 22:07:10 -0800

    requiring myApp

--- server.js
+++ server.js
@@ -7,8 +7,8 @@
 
 var bGround = require('fcc-express-bground');
 var fs = require('fs');
-var express = require('express');
 var myApp = require('./myApp');
+var express = require('express');
 var app = express();
 
 if (!process.env.DISABLE_XORIGIN) {
