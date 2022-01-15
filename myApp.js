efbb196aaf9b49714d7211988ababff40ee1b344
Hassan Abdinur <hassan.abdinur123@gmail.com>
2022-01-14 17:30:49 -0800

    changed html route to es6

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", function(req, res) {
+app.get("/json", (req, res) => {
     res.json(
         { "message": "Hello json" }
     );
