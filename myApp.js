9eb7adf233f09ff5df3813e9f37b6e2c25adb4c1
Hassan Abdinur <hassan.abdinur123@gmail.com>
2022-01-14 18:34:37 -0800

    changing middleware function to es6

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ var app = express();
 var bGround = require('fcc-express-bground');
 require('dotenv').config();
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 });
